43c4f66e8fc8e428af40511cfdf1251e6e74a745
nikhill03 <nikhilldev03@gmail.com>
2025-11-25 11:40:35 +0530

    Model Training completed

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -8,6 +8,7 @@ from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
 
 from src.components.data_transformation import DataTransformation,DataTransformationConfig
+from src.components.model_training import ModelTrainer,ModelTrainerConfig
 
 @dataclass   # for directing defining class variable instead of using init
 class DataIngestionConfig:
@@ -51,5 +52,8 @@ if __name__ == "__main__":
     obj = DataIngestion()
     train_data,test_data = obj.initiate_data_ingestion()
     
-    data_tranformation = DataTransformation()
-    data_tranformation.initiate_data_transformation(train_data,test_data)
+    data_transformation=DataTransformation()
+    train_arr,test_arr,_=data_transformation.initiate_data_transformation(train_data,test_data)
+    
+    modeltrainer=ModelTrainer()
+    print(modeltrainer.initiate_model_trainer(train_arr,test_arr))
